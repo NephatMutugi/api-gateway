98003f96b2859cc488ef1ec94936397fbe5bde9e
KEN20960 <muchirinephat5@gmail.com>
2022-09-30 16:58:33 +0200

    Change Routes

--- src/main/java/com/teohkenya/main/configuration/ApiGatewayConfiguration.java
+++ src/main/java/com/teohkenya/main/configuration/ApiGatewayConfiguration.java
@@ -1,14 +1,9 @@
 package com.teohkenya.main.configuration;
 
-import org.springframework.cloud.gateway.route.Route;
 import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.Buildable;
-import org.springframework.cloud.gateway.route.builder.PredicateSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.function.Function;
 
 /**
  * @ Author NMuchiri
@@ -20,15 +15,17 @@ public class ApiGatewayConfiguration {
     @Bean
     public RouteLocator gatewayRouteLocator(RouteLocatorBuilder builder) {
 
-        Function<PredicateSpec, Buildable<Route>> routeFunction =
-                p -> p.path("/get")
-                        .filters(f -> f
-                                .addRequestHeader("MyHeader", "MyURI"))
-                        .uri("http://httpbin.org:80");
-
         return builder
                 .routes()
-                .route(routeFunction)
+                .route(p -> p.path("/get")
+                        .filters(f -> f
+                                .addRequestHeader("MyHeader", "MyURI")
+                                .addRequestParameter("Param", "MyValue"))
+                        .uri("http://httpbin.org:80"))
+                .route(p -> p.path("/currency-exchange/**")
+                        .uri("lb://currency-exchange"))
+                .route(p -> p.path("/currency-conversion/**")
+                        .uri("lb://currency-conversion"))
                 .build();
     }
 }
