26c900ca9150e17b09b2e8d3404695c1e36de67b
KEN20960 <muchirinephat5@gmail.com>
2022-09-30 17:17:35 +0200

    Api Filters

--- src/main/java/com/teohkenya/main/configuration/LoggingFilter.java
+++ src/main/java/com/teohkenya/main/configuration/LoggingFilter.java
@@ -1,18 +1,28 @@
 package com.teohkenya.main.configuration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 /**
  * @ Author NMuchiri
  **/
+@Component
 public class LoggingFilter implements GlobalFilter {
 
+/*---------------------GLOBAL VARIABLES ----------------------*/
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        return null;
+
+        logger.info("Path of the request received ->{}",exchange.getRequest().getPath());
+
+        return chain.filter(exchange);
     }
 }
